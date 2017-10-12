e86d02e692e221dcc39bdbe353719360021db8b9
ConiX21 <nicolas.gasquet@nerys.biz>
2017-10-12 15:27:53 +0200

    add service product

---
+++ services/product-service.js
@@ -0,0 +1,75 @@
+(function () {
+    'use strict';
+
+    productApp.factory('productService', productService);
+
+    productService.$inject = [];
+    function productService() {
+        var products = [
+            {
+                "idProduct": 1,
+                "reference": "AA1230",
+                "description": "Consequat dolore proident pariatur anim laboris.",
+                "image": "/assets/img1.jpg",
+                "priceHT": 120.99
+            },
+            {
+                "idProduct": 2,
+                "reference": "AA4569",
+                "description": "Nostrud esse minim reprehenderit laborum et pariatur.",
+                "image": "/assets/img2.jpg",
+                "priceHT": 100
+            },
+            {
+                "idProduct": 3,
+                "reference": "AA5897",
+                "description": "Excepteur adipisicing proident adipisicing consectetur duis consequat et occaecat et labore.",
+                "image": "/assets/img3.jpg",
+                "priceHT": 50.20
+            },
+            {
+                "idProduct": 4,
+                "reference": "AA4512",
+                "description": "Fugiat culpa ex velit est in est nisi Lorem sit duis amet.",
+                "image": "/assets/img4.jpg",
+                "priceHT": 127.90
+            },
+            {
+                "idProduct": 5,
+                "reference": "AA4698",
+                "description": "Do non Lorem velit fugiat in dolor quis eiusmod velit et aliquip ex excepteur ea.",
+                "image": "/assets/img5.jpg",
+                "priceHT": 200.99
+            },
+            {
+                "idProduct": 6,
+                "reference": "AA1111",
+                "description": "Ex sint reprehenderit amet commodo minim.",
+                "image": "/assets/img6.jpg",
+                "priceHT": 500.60
+            },
+            {
+                "idProduct": 7,
+                "reference": "AA2367",
+                "description": "Ea ut non Lorem adipisicing commodo et proident deserunt dolore excepteur anim anim culpa.",
+                "image": "/assets/img7.jpg",
+                "priceHT": 80.99
+            },
+        ]
+
+        ////////////////
+        function details(id) {
+
+        }
+
+        function getAll() {
+            return products;
+        }
+
+        return {
+            details : details,
+            getAll : getAll
+        }
+    }
+})();
+
