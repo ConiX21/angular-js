71991337699f2128999934ca47a9663d93c77cbb
ConiX21 <nicolas.gasquet@nerys.biz>
2017-10-13 15:57:04 +0200

    Add form product

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -10,26 +10,40 @@
         $scope.stateWait = true;
         $scope.counter = 0;
         $scope.status = 200;
+        $scope.title = "Products";
 
         $scope.totalItems = 0;
         $scope.currentPage = 1;
         $scope.itemsPerPage = configProductApp.itemPerPage;
 
-        activate();
-
+        
         ////////////////
-
         if ($location.path().split('/').includes("details")) {
+            
             if (angular.isDefined($routeParams.id)) {
-               productService.getDetails($routeParams.id).then(
-                function(result){
-                    $scope.product = result.data
-                },function (response) {
-                    $scope.data = response.data || 'Request failed';
-                    $scope.status = response.status;
-                });
+                $scope.title = "Details product - " + $routeParams.id;
+                
+                productService.getDetails($routeParams.id).then(
+                    function (result) {
+                        $scope.product = result.data
+                    }, function (response) {
+                        $scope.data = response.data || 'Request failed';
+                        $scope.status = response.status;
+                    });
+                }
             }
-        }
+            else if ($location.path().split('/').includes("add")) {
+                $scope.title = "Add product";
+
+
+            }
+            else{
+
+                activate();
+            }
+
+
+        /*************Public Functions************/
 
         $scope.onDetails = function (id) {
             $timeout(function () {
@@ -45,6 +59,11 @@
             showDatasPagination();
         };
 
+        $scope.addProduct = function(product){
+            $log.info(product);
+        }
+
+        /*************Private Functions************/
         function activate() {
 
             var cpt = 0;
@@ -70,10 +89,10 @@
             productService.getPagination($scope.currentPage).then(function (result) {
                 $scope.products = result.data;
             },
-            function (response) {
-                $scope.data = response.data || 'Request failed';
-                $scope.status = response.status;
-            });
+                function (response) {
+                    $scope.data = response.data || 'Request failed';
+                    $scope.status = response.status;
+                });
         }
 
     }
